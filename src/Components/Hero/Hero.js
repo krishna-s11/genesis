b8d41a7aed7c96d788483f8902483abe91336da6
krishna-s11 <krishnasaxena69@gmail.com>
2023-02-22 00:03:58 +0530

    bug fixed

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,8 +7,10 @@ const Hero = () => {
     <div className="hero">
       <div className="hero_left">
         <h1 id="welcome">Welcome to</h1>
-        <h1 id="genesis">GENESIS</h1>
-        <p>"Creating value through compehensive solutions for energy sector"</p>
+        <h1 id="genesis">GENESIS ENERGY</h1>
+        <p>
+          "Creating value through compehrensive solutions for energy sector"
+        </p>
         <p id="getintouch">
           Get in Touch{" "}
           <BsArrowRightCircle style={{ transform: "translate(3px,2.5px)" }} />
